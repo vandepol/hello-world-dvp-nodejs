f1f0d33a95ea3ecdc8d8dc8fd103adc5bc21b16a
David VandePol <vandepol@ca.ibm.com>
2019-11-26 16:35:22 -0500

    Adding Error scenario for logging testing

--- src/services/hello-world.service.ts
+++ src/services/hello-world.service.ts
@@ -16,6 +16,18 @@ export class HelloWorldService implements HelloWorldApi {
 
   async greeting(name: string = 'World'): Promise<string> {
     this.logger.info(`Generating greeting for ${name}`);
+    this.logger.debug('This is a debug message from inside my API');
+    // Only Trigger error if the name passed in matches ERROR
+    if (name === 'ERROR' ) {
+        try {
+          throw new Error('"Throwing an error');
+        } catch (e) {
+          this.logger.error(e);
+          throw new Error('"Catching and Throwing errors');
+        } finally {
+          throw new Error('Last chance to trap this Error');
+        }
+    }
     return `Hello, ${name}!`;
   }
 }
